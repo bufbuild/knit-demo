be4cf8aeb5178f64f3004ba49a0eef9722e9bd11
Joshua Humphries <2035234+jhump@users.noreply.github.com>
2023-10-05 10:27:58 -0400

    Run tests and lint, including simple integration test, in CI (#5)
    Previously, CI was only to auto-deploy the demo app to
    https://knit-demo.connect.build.
    
    But it should also run tests and perform linting, to make sure a PR
    doesn't break stuff, and it should push to the BSR when the protos
    change. So this adds that. It also adds a simple integration test that
    starts up the server, a knit gateway, and then runs the `ts/index.ts`
    script to hit the gateway and make sure a query works.

--- example/start.sh
+++ example/start.sh
@@ -4,13 +4,15 @@ set -e
 
 cd "$(dirname $0)"
 
-go build -o swapi-server ../go/cmd/swapi-server
-go build -o knitgateway github.com/bufbuild/knit-go/cmd/knitgateway
+export GOBIN=$(dirname $PWD)/.tmp/bin
+mkdir -p $GOBIN
+go install ../go/cmd/swapi-server
+go install github.com/bufbuild/knit-go/cmd/knitgateway@${KNIT_GO_VERSION:=v0.1.0}
 
 buf build buf.build/bufbuild/knit-demo \
     --type buf.knit.demo.swapi.planet.v1.PlanetService \
     --type buf.knit.demo.swapi.relations.v1.PlanetResolverService \
-    -o planetsvc.protoset
+    -o ../.tmp/planetsvc.protoset
 
 function cleanup() {
   for pid in $pids; do
@@ -28,28 +30,28 @@ function run_server() {
   exec "$@"
 }
 
-run_server "   film" ./swapi-server -port 30481 \
+run_server "   film" $GOBIN/swapi-server -port 30481 \
     -service "buf.knit.demo.swapi.film.v1.FilmService" \
     -service "buf.knit.demo.swapi.relations.v1.FilmResolverService" &
 pids="$!"
-run_server " person" ./swapi-server -port 30482 \
+run_server " person" $GOBIN/swapi-server -port 30482 \
     -service "buf.knit.demo.swapi.person.v1.PersonService" \
     -service "buf.knit.demo.swapi.species.v1.SpeciesService" \
     -service "buf.knit.demo.swapi.relations.v1.PersonResolverService" \
     -service "buf.knit.demo.swapi.relations.v1.SpeciesResolverService" &
 pids="$pids $!"
-run_server " planet" ./swapi-server -port 30483 \
+run_server " planet" $GOBIN/swapi-server -port 30483 \
     -service "buf.knit.demo.swapi.planet.v1.PlanetService" \
     -service "buf.knit.demo.swapi.relations.v1.PlanetResolverService" &
 pids="$pids $!"
-run_server "vehicle" ./swapi-server -port 30484 \
+run_server "vehicle" $GOBIN/swapi-server -port 30484 \
     -service "buf.knit.demo.swapi.starship.v1.StarshipService" \
     -service "buf.knit.demo.swapi.vehicle.v1.VehicleService" \
     -service "buf.knit.demo.swapi.relations.v1.StarshipResolverService" \
     -service "buf.knit.demo.swapi.relations.v1.VehicleResolverService" &
 pids="$pids $!"
 
-run_server "gateway" ./knitgateway -conf knitgateway.swapi-micro.yaml &
+run_server "gateway" $GOBIN/knitgateway -conf knitgateway.swapi-micro.yaml &
 pids="$pids $!"
 
 for pid in $pids; do
---
+++ integration-test.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname $0)"
+
+export GOBIN=$PWD/.tmp/bin
+mkdir -p $GOBIN
+go install ./go/cmd/swapi-server
+go install github.com/bufbuild/knit-go/cmd/knitgateway@${KNIT_GO_VERSION:=v0.1.0}
+
+curl -o ./.tmp/knitgateway.yaml https://raw.githubusercontent.com/bufbuild/knit-go/${KNIT_GO_VERSION}/knitgateway.example.yaml
+
+function cleanup() {
+  for pid in $pids; do
+    kill $pid 2>/dev/null || true
+  done
+}
+
+trap cleanup EXIT
+
+function run_server() {
+  server_name="$1"
+  shift
+  exec > >(trap "" INT TERM; sed 's/^/'"$server_name"': /')
+  exec 2> >(trap "" INT TERM; sed 's/^/'"$server_name"': /' >&2)
+  exec "$@"
+}
+
+run_server "  swapi" $GOBIN/swapi-server &
+pids="$!"
+
+run_server "gateway" $GOBIN/knitgateway -conf ./.tmp/knitgateway.yaml &
+pids="$pids $!"
+
+# We want to make sure above servers are up and running before we
+# run the next step. So give it a second (literally).
+sleep 1
+
+cd ts
+npm install
+npm run start
